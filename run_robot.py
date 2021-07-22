71c1aa916fa5dda087ecefed5a0e4429ffbf7120
Eric Yang <ericy61101@gmail.com>
2021-07-22 16:19:47 -0700

    adding positions to portfolio

--- run_robot.py
+++ run_robot.py
@@ -27,3 +27,60 @@ trading_robot = PyRobot(
     trading_account=ACCOUNT_NUMBER,
     paper_trading=True
 )
+
+# Create a new portfolio
+trading_robot_portfolio = trading_robot.create_portfolio()
+
+# Add multiple positions to our portfolio
+multi_position = [
+    {
+        'asset_type': 'equity',
+        'quantity': 2,
+        'purchase_price': 4.00,
+        'symbol': 'TSLA',
+        'purchase_date': '2020-01-31'
+    },
+    {
+        'asset_type': 'equity',
+        'quantity': 2,
+        'purchase_price': 4.00,
+        'symbol': 'SQ',
+        'purchase_date': '2020-01-31'
+    }
+]
+
+# Add those positions to the portfolio
+new_positions = trading_robot.portfolio.add_positions(positions=multi_position)
+pprint.pprint(new_positions)
+
+#Add a single position to the portfolio
+trading_robot.portfolio.add_position(
+    symbol='MSFT',
+    quantity=10,
+    purchase_price=10.00,
+    assest_type='equity',
+    purchase_date='2020-04-01'
+)
+pprint.pprint(trading_robot.portfolio.positions)
+
+# Check to see if the regular market is open
+if trading_robot.regular_market_open:
+    print('Regular Market Open')
+else:
+    print('Regular Market Not Open')
+
+# Check to see if the pre market is open
+if trading_robot.pre_market_open:
+    print('Pre Market Open')
+else:
+    print('Pre Market Not Open')
+
+# Check to see if the post market is open
+if trading_robot.post_market_open:
+    print('Post Market Open')
+else:
+    print('Post Market Not Open')
+
+# Grab the current quotes in our portfolio
+current_quotes = trading_robot.grab_current_quotes()
+pprint.pprint(current_quotes)
